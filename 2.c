97077048e434d47d440589fc5dd201cb1be7db1b
mellokx <hernanvicentecaro@gmail.com>
2021-12-10 23:44:14 -0300

    loop unrolling aplicado
    Co-authored-by: vane000 <vane000@users.noreply.github.com>

--- 2.c
+++ 2.c
@@ -29,23 +29,28 @@ int f2 (struct X *p, char *s) {
         len = strlen(p->a);
     else
         len = strlen(s);
-    char local_pa [256];
-    char local_s [256];
     printf("len=%d\n",len);
-    for (int i = 0; i<len ; ++i)
-        if (!(local_pa[i] == local_s[i])){
+    for (int i = 0; i<len ; i+=2){
+        if (!(p->a[i] == s[i])){
             t++;
         }
+        if ((i != len - 1) || (len%2 == 0)){
+            if(!(p->a[i+1] == s[i+1])){
+                t++;
+            }
+        }
+    }
     return p->b + t;
 }
 
 int main(){
     struct X * x = malloc(sizeof(struct X));
     char * ss = malloc(256*sizeof(char));
-    strcpy(x->a,"palabra");
-    //strcpy(ss,  "palabra");
+    strcpy(x->a,"holamama");
+    strcpy(ss,  "holamama");
     x->b = 0;
-    printf("Resultado f(): %d\n",f(x,x->a));
+    printf("Resultado f(): %d\n",f(x,ss));
+    printf("Resultado f2(): %d\n",f2(x,ss));
     printf("Press Enter to Continue\n"); 
     while(getchar() != '\n'); 
     free(x);
