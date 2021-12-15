9061d85e66e1a15c2b5b6a5252a1268a561ed014
mellokx <hernanvicentecaro@gmail.com>
2021-12-15 17:50:03 -0300

    ideas random

--- 2.c
+++ 2.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 struct X {
-    char a[256];
+    char a[500000];
     int b;
 };
 
@@ -14,7 +15,6 @@ int f (struct X *p, char *s) {
         len = strlen(p->a);
     else
         len = strlen(s);
-    printf("len=%d\n",len);
     for (int i = 0; i<len ; ++i)
         if (!(p->a[i] == s[i])){
             t++;
@@ -25,21 +25,27 @@ int f (struct X *p, char *s) {
 int f2 (struct X *p, char *s) {
     int t = 0;
     int len = 0;
-    if( strlen(s) > strlen(p->a))
-        len = strlen(p->a);
+    int size_s = strlen(s);
+    int size_pa = strlen(p->a);
+    char cadena_s [size_s + 1];
+    char cadena_pa [size_pa + 1];
+    strcpy(cadena_s,s);
+    strcpy(cadena_pa,p->a);
+    if( size_s > size_pa)
+        len = size_pa;
     else
-        len = strlen(s);
-    printf("len=%d\n",len);
+        len = size_s;
+    /*Aplicamos loop unrolling */
     for (int i = 1; i<len ; i+=2){
-        if (!(p->a[i-1] == s[i-1])){
+        if (!(cadena_pa[i-1] == cadena_s[i-1])){
             t++;
         }
-        if (!(p->a[i] == s[i])){
+        if (!(cadena_pa[i] == cadena_s[i])){
             t++;
         }
     }
     if (len%2 != 0){
-        if(!(p->a[len - 1] == s[len - 1])){
+        if(!(cadena_pa[len - 1] == cadena_s[len - 1])){
             t++;
         }
     }
@@ -47,15 +53,33 @@ int f2 (struct X *p, char *s) {
 }
 
 int main(){
+    FILE * fichero;
+    fichero = fopen("texto1.txt","r");
+    char cadena_local[500000];
+    if( fgets(cadena_local,500000,fichero) != NULL ){
+        int tam =strlen(cadena_local);
+        printf("Texto Guardado! con size = %d\n",tam);
+    }
     struct X * x = malloc(sizeof(struct X));
-    char * ss = malloc(256*sizeof(char));
-    strcpy(x->a,"abcd");
-    strcpy(ss,  "1234");
+    char * ss = malloc(500000*sizeof(char));
+    strcpy(x->a,cadena_local);
+    strcpy(ss,  cadena_local);
     x->b = 0;
-    printf("Resultado f(): %d\n",f(x,ss));
-    printf("Resultado f2(): %d\n",f2(x,ss));
-    printf("Press Enter to Continue\n"); 
-    while(getchar() != '\n'); 
+    double time = 0;
+    clock_t start1 = clock();
+    printf("resultado f():%d",f(x,ss));
+    clock_t end1 = clock();
+    time = (double)(end1 - start1)/ CLOCKS_PER_SEC;
+    printf("tiempo de f = %f segundos\n",time);
+    time = 0;
+    clock_t start2 = clock();
+    printf("resultado f():%d",f2(x,ss));
+    clock_t end2 = clock();
+    time = (double) (end2 - start2)/ CLOCKS_PER_SEC;
+    printf("tiempo de f2 = %f segundos\n",time);
+    //printf("Press Enter to Continue\n"); 
+    //while(getchar() != '\n'); 
+
     free(x);
     free(ss);
     return 0;
